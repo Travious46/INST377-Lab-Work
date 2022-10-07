a7b9f08370f1ba12887af067ac37de821ee9a8bc
Travious46 <46880742+Travious46@users.noreply.github.com>
2022-10-07 18:13:17 -0400

    Added script tag + array_demo.js + updated script.js

---
+++ utils/array_demo.js
@@ -0,0 +1,9 @@
+const array = [1,2,3,4,5,6];
+
+function multiplyByTwo(x) {
+  return x * 2;
+}
+
+const twoFer = array.map((item, idx) => multiplyByTwo(item));
+
+const newFer = array.filter((item, idx) => item % 3 === 0);
